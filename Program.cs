39f97cedb7097c9cf49de8455ceb9f406a07bf2a
onurcoskun1 <coskunonur01@gmail.com>
2022-02-11 16:21:56 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time= DateTime.Now.Hour;
+
+            if (time>=6 && time<= 11)
+                Console.WriteLine("Günaydın!");
+            else if (time <= 18)
+                Console.WriteLine("İyi Günler!");
+            else 
+                Console.WriteLine("İyi Geceler");
+        }
+    }
+}
